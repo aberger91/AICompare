0f763fec4de434b4bdb2e56c2453662c46bdf944
Andrew.Berger <Andrew.Berger@intlfcstone.com>
2018-03-05 13:26:05 -0600

    add plot

--- ML/make_arff.py
+++ ML/make_arff.py
@@ -1,18 +1,23 @@
 import glob
+import pickle
 from scipy.io import arff
 from nltk.stem import WordNetLemmatizer
+from nltk import download
 
 STOPWORDS_PATH = 'books/stopwords.txt'
 PUNCTUATION = ['"', ':', ',', '.', '!', '?', ';', '(', ')', '\'s', '\'']
 STOPWORDS = open(STOPWORDS_PATH).read().strip('\n').split('\n\n')
+WORDNET_LEMMATIZER = WordNetLemmatizer()
 
-lemmatizer = WordNetLemmatizer()
+def lemmatize(s):
+    return WORDNET_LEMMATIZER.lemmatize(s)
 
 def get_files():
     text_files = glob.glob('books/*.txt')
     files = { }
     for f in text_files:
-        key = f.replace('.txt', '')
+        key = f.split('\\')[-1]
+        key = key.replace('.txt', '')
         if key != 'stopwords':
             data = open(f).read().split('\n\n') # split by paragraph
             files[key] = data
@@ -29,9 +34,11 @@ def check_stopword_add_(word, li):
     word = remove_punctuation(word).lower()
     if word in STOPWORDS or word == '':
         return li
-    lemma_word = lemmatizer.lemmatize(word)
-    if word in STOPWORDS:
+    lemmatized_word = lemmatize(word)
+    #print('Lemmatizing: (%s) => (%s)' % (word, lemmatized_word), end='\r')
+    if lemmatized_word in STOPWORDS:
         return li
+    li += [ lemmatized_word ]
     return li
 
 def remove_stopwords(paragraph):
@@ -60,23 +67,33 @@ def get_word_counts(files):
                     counts[w] = 1
     return counts
 
-def make_arff_meta(name, files, attributes, samples=250):
+def make_arff_meta(name, files, attributes):
+    print('Creating arff metadata ...')
     file_ = '@relation %s\n\n' % name
+    print('Adding Relation: %s' % file_, end='\r')
     file_ += '@attribute %s {' % name
     for title, book in files.items():
         file_ += title + ', '
-
     file_ = file_.strip(', ')
     file_ += '}\n\n'
     for word, _ in attributes:
-        file_ += '@attribute ' + word + ' numeric\n'#' {0, 1}\n'
+        s = '@attribute ' + word + ' {0, 1}\n'
+        print('Adding Attribute: %s' % s, end='\r')
+        file_ += s
     return file_
 
-def make_arff(dest_path, n=250):
-    def make_arff_data_row(title, paragraph, dict_):
-        record = '%s,' % title
-        for name, count in dict_:
+def get_sampled_sorted_word_list(files, n):
+    word_counts = get_word_counts(files)
+    sorted_words = sorted(word_counts.items(), key=lambda x: x[1])
+    sampled_sorted_words = sorted_words[-(n-1):]
+    f = open('wordcount.pylist', 'wb')
+    pickle.dump(sampled_sorted_words, f)
+    return sampled_sorted_words
 
+def make_arff(dest_path, n):
+    def make_arff_data_row(title, paragraph, li):
+        record = '%s,' % title
+        for name, count in li:
             if name in paragraph:
                 record += '1,'
             else:
@@ -85,31 +102,39 @@ def make_arff(dest_path, n=250):
         return record
 
     files = get_files()
-    word_counts = get_word_counts(files)
 
-    sorted_dict = sorted(word_counts.items(), key=lambda x: x[1])
-    sampled_sorted_dict = sorted_dict[-(n-1):]
-    file_ = make_arff_meta('_author_', files, sampled_sorted_dict, samples=n)
+    print('Generating arff file with %d features' % n)
+    sampled_sorted_words = get_sampled_sorted_word_list(files, n)
+    file_ = make_arff_meta('_author_', files, sampled_sorted_words)
 
     file_ += '''\n@data\n'''
     for title, book in files.items():
         for paragraph in book:
-            record = make_arff_data_row(title, paragraph, sampled_sorted_dict)
+            record = make_arff_data_row(title, paragraph, sampled_sorted_words)
             file_ += record + '\n'
-    return file_
-
-def create_and_load_arff(dest_path, n=250):
-    file_ = make_arff(dest_path, n=n)
     with open(dest_path, 'w') as f:
         f.write(file_)
+    return file_
+
+def load_arff(path):
     try:
-        arff_file = arff.loadarff(dest_path)
+        arff_file = arff.loadarff(path)
     except Exception as e:
-        raise Exception('could not create a valid arff file ')
+        raise Exception('could not load a valid arff file ')
     return arff_file
 
+def main(argc, argv):
+    if argc < 2:
+        print('usage: make_arff.py <int>')
+        return
+    else:
+        n = argv[1]
+    download('wordnet')
+    arff_path = 'data.arff'
+    make_arff(arff_path, int(n))
+    load_arff(arff_path)
 
 if __name__ == '__main__':
-    #main()
-    create_and_load_arff('data.arff')
+    import sys
+    main(len(sys.argv), sys.argv)
 
